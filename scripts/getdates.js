cf9ab4aa72af478f581c3112efb23b8627b992ed
Dougmanstew <dougmanstew@gmail.com>
2024-11-08 23:41:17 -0700

    week 1 done

---
+++ scripts/getdates.js
@@ -0,0 +1,9 @@
+var date = new Date();
+var year = date.getFullYear();
+
+document.getElementById('year').innerHTML = (year);
+
+
+let text = document.lastModified;
+
+document.getElementById("mod").innerHTML = text;
